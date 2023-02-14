c4f8dfcf904bb0db4606efc8f1a5eeb8d11732de
Tanmoy Sen Gupta <tanmoysps@gmail.com>
2023-02-15 01:37:03 +0530

    Added queryResponse parse methods

--- internal/queries/queries.go
+++ internal/queries/queries.go
@@ -6,19 +6,21 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+
+	"github.com/TanmoySG/wdb-go/internal/queries/response"
 )
 
 const (
-	queryErrorFormat = "error processing query: %s"
+	queryErrorFormat    = "error processing query: %s"
+	responseErrorFormat = "error processing response: %s"
 
-	contentType = "application/json"
-
+	contentType       = "application/json"
 	contentTypeHeader = "Content-Type"
 
-	userAgent = "User-Agent"
+	userAgentHeader = "User-Agent"
 )
 
-type queryResponse map[string]interface{}
+type queryResponse []byte
 
 type QueryClient struct {
 	client         http.Client
@@ -29,6 +31,19 @@ type QueryClient struct {
 type authentication struct {
 	username string
 	password string
+}
+
+func NewQueryClient(username, password, userAgent string) QueryClient {
+	qc := QueryClient{
+		client: *http.DefaultClient,
+		authentication: authentication{
+			username: username,
+			password: password,
+		},
+		userAgent: userAgent,
+	}
+
+	return qc
 }
 
 func (qc QueryClient) Query(endpoint, method string, payload interface{}) (queryResponse, error) {
@@ -47,8 +62,8 @@ func (qc QueryClient) Query(endpoint, method string, payload interface{}) (query
 	request.SetBasicAuth(qc.authentication.username, qc.authentication.password)
 
 	// User Agent set to wdb-go-client-<project name>
-	request.Header.Set(userAgent, qc.userAgent)
-	
+	request.Header.Set(userAgentHeader, qc.userAgent)
+
 	// Content Type
 	request.Header.Set(contentTypeHeader, contentType)
 
@@ -59,14 +74,26 @@ func (qc QueryClient) Query(endpoint, method string, payload interface{}) (query
 
 	responseBody, err := io.ReadAll(response.Body)
 	if err != nil {
-		return nil, fmt.Errorf(queryErrorFormat, err)
+		return nil, fmt.Errorf(responseErrorFormat, err)
 	}
 
-	var queryResponse queryResponse
-	err = json.Unmarshal([]byte(responseBody), &queryResponse)
+	return responseBody, nil
+}
+
+func (qr queryResponse) Map() (map[string]interface{}, error) {
+	var responseMap map[string]interface{}
+	err := json.Unmarshal(qr, &responseMap)
 	if err != nil {
-		return nil, fmt.Errorf(queryErrorFormat, err)
+		return nil, fmt.Errorf(responseErrorFormat, err)
 	}
+	return responseMap, nil
+}
 
-	return queryResponse, nil
+func (qr queryResponse) ApiResponse() (*apiResponse.Response, error) {
+	var apiResponse apiResponse.Response
+	err := json.Unmarshal(qr, &apiResponse)
+	if err != nil {
+		return nil, fmt.Errorf(responseErrorFormat, err)
+	}
+	return &apiResponse, nil
 }
