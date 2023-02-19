a4e47d26ebc4e5d9a19c429dde2344a91a5d5120
Tanmoy Sen Gupta <tanmoysps@gmail.com>
2023-02-19 13:33:07 +0530

    Added CreateRole model

--- models/user_models.go
+++ models/user_models.go
@@ -4,3 +4,9 @@ type CreateUser struct {
 	Username string `json:"username" xml:"username" form:"username"`
 	Password string `json:"password" xml:"password" form:"password"`
 }
+
+type CreateRole struct {
+	Role    string   `json:"role" xml:"role" form:"role"`
+	Allowed []string `json:"allowed" xml:"allowed" form:"allowed"`
+	Denied  []string `json:"denied" xml:"denied" form:"denied"`
+}
